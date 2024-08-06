d2ec3e50a7ed7c926894c169a8c837a9f89c9f7f
Artem Khazanov <tulenka04@gmail.com>
2024-08-06 21:23:33 +0300

    init commit

---
+++ src/test/java/telran/range/BrokenFloorTest.java
@@ -0,0 +1,35 @@
+package telran.range;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class BrokenFloorTest {
+
+    private int getMinimalBrokenFloor(BallBrokenFloor bbf) {
+        int downFloor = 0;
+        int upFloor = bbf.getNFloors();
+        int mid = (downFloor + upFloor) / 2;
+        int res = -1;
+        while (downFloor <= upFloor) {
+            try {
+                bbf.checkFloor(mid);
+                downFloor = mid + 1;
+            } catch (Exception e) {
+                upFloor = mid - 1;
+                res = mid;
+            }
+            mid = (downFloor + upFloor) / 2;
+        }
+        return res;
+    }
+
+    @Test
+    void minimalBrokenFloorTest() {
+        int[] floors = { 200, 17, 1001, 2000 };
+        for (int i = 0; i < floors.length; i++) {
+            BallBrokenFloor bbf = new BallBrokenFloor(floors[i]);
+            assertEquals(bbf.getMinBrokenFloor(), getMinimalBrokenFloor(bbf));
+        }
+    }
+}
---
+++ src/test/java/telran/range/RangeTest.java
@@ -0,0 +1,31 @@
+package telran.range;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import telran.range.exceptions.OutOfRangeMaxValueException;
+import telran.range.exceptions.OutOfRangeMinValueException;
+
+public class RangeTest {
+    private static final int MIN = 50;
+    private static final int MAX = 100;
+    Range range = Range.getRange(MIN, MAX);
+
+    @Test
+    void wrongRangeCreatingTest() {
+        assertThrowsExactly(IllegalArgumentException.class, () -> Range.getRange(MAX, MIN));
+    }
+
+    @Test
+    void rightNumberTest() throws OutOfRangeMaxValueException, OutOfRangeMinValueException {
+        range.checkNumber(55);
+    }
+
+    @Test
+    void wrongNumberTest() throws Exception {
+        range.checkNumber(55);
+        assertThrowsExactly(OutOfRangeMaxValueException.class, () -> range.checkNumber(MAX + 1));
+        assertThrowsExactly(OutOfRangeMinValueException.class, () -> range.checkNumber(MIN - 1));
+    }
+}
